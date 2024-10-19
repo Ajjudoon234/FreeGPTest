5d393a7151eb9e0575ef52626430488f2feec798
Ajjudoon234 <kaludoon143@gmail.com>
2024-10-19 15:02:53 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/logtim.py",
+    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/snyc.py",
     "customizations": {
         "vscode": {
             "settings": {
